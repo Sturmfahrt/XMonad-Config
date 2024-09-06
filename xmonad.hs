f31c0a73dde82a4cc4c5ab56524f47b0e56213e6
Sturmfahrt <stormfart@proton.me>
2024-09-05 23:53:14 -0400

    fixed the issue. turns out the issue was that the fucking package for xmonad on nixos was broken in the just right way to make me pull my hair out. as a result i had to tell home-manager to use a newer GHC package to force it to pull a newer version of xmonad. as a result it compiles a known good version of xmonad thereby unfucking itself. no more dependency twilight zone, Horray! Thank you random strangers on discord for helping my get this out of FUBAR status. also i forgot to commit when i fixed the issue, so i already started working on the layout thing based on the documentation.

--- xmonad.hs
+++ xmonad.hs
@@ -1,15 +1,27 @@
 --{-# OPTIONS_GHC -Wno-deprecations #-}
 import XMonad
 import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
 import XMonad.Layout.ThreeColumns
+import Distribution.InstalledPackageInfo (showFullInstalledPackageInfo)
+import GHC.Real (ratioPrec)
 
 main :: IO ()
 main = xmonad $ def
-    { modMask = mod4Mask  -- Rebind Mod to the Super key
+    {
+      modMask = mod4Mask  -- Rebind Mod to the Super key
+    , layoutHook = myLayout
     }
   `additionalKeysP`
     [ ("M-S-z", spawn "xscreensaver-command -lock")
     , ("M-C-s", unGrab *> spawn "scrot -s"        )
     , ("M-f"  , spawn "floorp"                    )
+    , ("M-e"  , spawn "thunar"                    )
     ]
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled = Tall nmaster delta ratio
+    nmaster = 1     -- Default number of windows in the master pane
+    ratio = 1/2     -- Default proportion of screen occupied by master pane
+    delta = 3/100   -- Percent of screen to increment by when resizing panes
